010249566223b40497de0c255d226b7b68cc158b
xs9 <61426269+Xsintashi@users.noreply.github.com>
2023-08-20 18:18:06 +0200

    Update Studio.h

--- Azurre/SDK/Studio.h
+++ Azurre/SDK/Studio.h
@@ -4,22 +4,22 @@
 #include "Vector.h"
 
 struct StudioHdr {
-	int id;
-	int version;
-	int checksum;
-	PAD(64)
-	int length;
-	Vector eyeposition;
-	Vector illumposition;
-	Vector hull_min;
-	Vector hull_max;
-	Vector view_bbmin;
-	Vector view_bbmax;
-	int flags;
-	int numbones;                // bones
-	int boneindex;
-	int numbonecontrollers;      // bone controllers
-	int bonecontrollerindex;
-	int numhitboxsets;
-	int hitboxsetindex;
+    int id;
+    int version;
+    int checksum;
+    char name[64];
+    int length;
+    Vector eyePosition;
+    Vector illumPosition;
+    Vector hullMin;
+    Vector hullMax;
+    Vector bbMin;
+    Vector bbMax;
+    int flags;
+    int numBones;
+    int boneIndex;
+    int numBoneControllers;
+    int boneControllerIndex;
+    int numHitboxSets;
+    int hitboxSetIndex;
 };
