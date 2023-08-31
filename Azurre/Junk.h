4396d7b18ee0a222ab5c82dc1dc23ad4d7703506
xs9 <61426269+Xsintashi@users.noreply.github.com>
2023-08-31 19:52:37 +0200

    Update Junk.h

--- Azurre/Junk.h
+++ Azurre/Junk.h
@@ -10,11 +10,16 @@
 	__asm _emit 0xEB \
 	__asm _emit 0x06 \
 	__asm _emit 0xAA \
-	__asm _emit 0xEE \
-	__asm _emit 0xFF \
-	__asm _emit 0xBB \
-	__asm _emit 0xDD \
+	__asm _emit 0xEE \ // You can change bytes below
+	__asm _emit 0x1F \
+	__asm _emit 0xAB \
+	__asm _emit 0x0D \
 	__asm _emit 0xCC \
+	__asm _emit 0x0E \
+	__asm _emit 0x13 \
+	__asm _emit 0xA3 \
+	__asm _emit 0xC0 \
+	__asm _emit 0x87 \
 } ;
 
 #endif
